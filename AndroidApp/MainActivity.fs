163508fdae4e8eccd507fc6c0c14b9bee48dd5fc
nosami <jasonimison@gmail.com>
2018-10-13 10:43:41 +0100

    Add sample project for testing

---
+++ AndroidApp/MainActivity.fs
@@ -0,0 +1,26 @@
+﻿namespace AndroidApp
+
+open System
+
+open Android.App
+open Android.Widget
+
+[<Activity (Label = "AndroidApp", MainLauncher = true, Icon = "@mipmap/icon")>]
+type MainActivity () =
+    inherit Activity ()
+
+    let mutable count:int = 1
+
+    override this.OnCreate (bundle) =
+
+        base.OnCreate (bundle)
+
+        // Set our view from the "main" layout resource
+        this.SetContentView (Resources.Layout.Main)
+
+        // Get our button from the layout resource, and attach an event to it
+        let button = this.FindViewById<Button>(Resources.Id.sampleButton)
+        button.Click.Add (fun args -> 
+            button.Text <- sprintf "%d clicks!" count
+            count <- count + 1
+        )
